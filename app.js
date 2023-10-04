26a4af61ae7e6806d35163cbd7f33f98d283615a
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2023-10-04 13:14:29 -0400

    bmi calculator

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const num1 = Number(prompt("Enter your weight, in kilograms."));
 const num2 = Number(prompt("Enter your height, in meters."));
 
 function bmi_calc(x, y){
-    z = y ** 2;
-    bmi = x / y;
+    return (x/y ** 2);
 }
-const bmi = bmi_calc(num1, num2);
-let result;
-if (bmi < 18.5){
-    result = "You are underweight.";
-} else if (bmi > 24.9){
-    console.log("You are overweight.");
-} else {
-    console.log("You are healthy weight.");
+
+function bmi_log(){
+    const bmi = bmi_calc(num1,num2);
+    console.log(bmi);
+    if (bmi < 18.5){
+        console.log("You are underweight.");
+    } else if (bmi > 24.9){
+        console.log("You are overweight.");
+    } else {
+        console.log("You are healthy weight.");
+    }
 }
+
+bmi_log();
